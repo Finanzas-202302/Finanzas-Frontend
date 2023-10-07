3181932f197a2bcbbc866c94cb565dd975f529dc
Sofia Alfaro <alfaros.sofia@gmail.com>
2023-10-07 01:00:24 -0500

    Update: Routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResultsComponent } from './components/results/results.component';
+import { CalculateviewComponent } from './components/calculateview/calculateview.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'results', component: ResultsComponent},
+  { path: 'calculate', component: CalculateviewComponent},
+  { path: 'clients', component: ClientsComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: '**', component: NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

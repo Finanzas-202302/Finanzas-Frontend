f0c76d765134ff8a47a7aa6f8ee473b29fd2cc69
Sofia Alfaro <alfaros.sofia@gmail.com>
2023-10-22 00:16:25 -0500

    Update: Home grid

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  tiles_why_us: Tile[] = [
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 2, rows: 1, color: 'lightpink'},
+  ];
+  tiles_global: Tile[] = [
+    {text: 'One', cols: 2, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+  ];
+  tiles_what_we_do: Tile[] = [
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Five', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Six', cols: 1, rows: 1, color: 'lightpink'},
+  ];
 }
